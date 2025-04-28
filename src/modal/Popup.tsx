0ad685dfe4978f633576d8516531bc56ae120c1f
Jeff <jef.yabut.18@gmail.com>
2025-04-28 17:37:09 +0800

    On dev

---
+++ src/modal/Popup.tsx
@@ -0,0 +1,39 @@
+import { useEffect, useRef } from "react";
+import { graphicTypes } from "../types/goated";
+
+export default function Popup({ isOpen, onClose, children }: graphicTypes) {
+  const modalRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    function handleClickOutside(e: MouseEvent) {
+      if (modalRef.current && !modalRef.current.contains(e.target as Node)) {
+        onClose();
+      }
+    }
+    if (isOpen) {
+      document.addEventListener("mousedown", handleClickOutside);
+    }
+    return () => document.removeEventListener("mousedown", handleClickOutside);
+  }, [isOpen, onClose]);
+
+  return (
+    <>
+      {isOpen && (
+        <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50 backdrop-blur-sm">
+          <div
+            ref={modalRef}
+            className="bg-white dark:bg-gray-700 rounded-2xl p-6 max-w-md w-full relative h-max overflow-y-auto text-text"
+          >
+            <button
+              onClick={onClose}
+              className="absolute top-2 right-3 text-gray-500 dark:text-darkText hover:text-gray-800"
+            >
+              ✕
+            </button>
+            {children}
+          </div>
+        </div>
+      )}
+    </>
+  );
+}
---
+++ src/types/goated.ts
@@ -0,0 +1,5 @@
+export type graphicTypes = {
+  isOpen: string | null | boolean;
+  onClose: () => void;
+  children: React.ReactNode;
+};
