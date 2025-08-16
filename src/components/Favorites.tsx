6bef196900ed226e581082df29d42f396f419531
Jeff <jef.yabut.18@gmail.com>
2025-08-16 11:02:36 +0800

    On Dev

--- src/components/Favorites.tsx
+++ src/components/Favorites.tsx
@@ -1,44 +1,10 @@
-import movie1 from "/images/Flow.jpg";
-import movie2 from "/images/WildRobot.jpg";
-import movie3 from "/images/Elemental.jpg";
-import movie4 from "/images/Eli.jpg";
-import movie5 from "/images/Raya.jpg";
-import movie6 from "/images/Dinosaur.jpg";
-
 import { useRef } from "react";
 import Scrolls from "../modal/Scrolls";
-import { Movie } from "../types/goated";
 import Mapping from "./Mapping";
+import imageData from "../data/images.json";
 
 export default function Favorites() {
   const ref = useRef<HTMLDivElement>(null);
-
-  const movies: Movie[] = [
-    {
-      src: movie1,
-      rating: "9.5",
-    },
-    {
-      src: movie2,
-      rating: "9.5",
-    },
-    {
-      src: movie3,
-      rating: "10",
-    },
-    {
-      src: movie4,
-      rating: "10",
-    },
-    {
-      src: movie5,
-      rating: "8.5",
-    },
-    {
-      src: movie6,
-      rating: "10",
-    },
-  ];
 
   const scrollLeft = () => {
     if (ref.current) {
@@ -68,10 +34,10 @@ export default function Favorites() {
             ref={ref}
             style={{ scrollBehavior: "smooth" }}
           >
-            {movies.map((movie, index) => (
+            {imageData.map((movie, index) => (
               <Mapping
                 containerClass="h-40 xl:h-96 ml-1 mr-5 rounded-md hover:p-2 hover:rounded drop-shadow flex items-center justify-center "
-                src={movie.src}
+                src={`/${movie.path}`}
                 rating={movie.rating}
                 index={index}
                 key={index}
